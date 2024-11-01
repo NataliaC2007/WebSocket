c5d49bc6bbb1b77b26c63bb4c0effceae73c3633
NataliaC2007 <nataliacostaaraujo5@gmail.com>
2024-11-01 07:34:58 -0300

    Estilizando e arrumando o código

--- client/main.js
+++ client/main.js
@@ -1,13 +1,10 @@
-// main.js
 const socket = new WebSocket("ws://localhost:8080");
 
-// Quando a conexão é estabelecida, solicita o nome de usuário
 socket.onopen = () => {
   const nomeUsuario = prompt("Digite seu nome de usuário:");
   socket.send(nomeUsuario);
 };
 
-// Exibe no console as mensagens recebidas do servidor
 socket.onmessage = (event) => {
   console.log("Mensagem do servidor:", event.data);
 };
